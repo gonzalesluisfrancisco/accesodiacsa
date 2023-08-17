cc25074e57d9833727c77f4c733ecb2ab4b9b30f
gonzalesluisfrancisco <gonzales.luisf@gmail.com>
2023-08-17 10:08:15 -0500

    Update settings.py
    Available whitenoise

--- controlaesadiacsa/settings.py
+++ controlaesadiacsa/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = 'static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Turn on WhiteNoise storage backend that takes care of compressing static files
 # and creating unique names for each version so they can safely be cached forever.
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
