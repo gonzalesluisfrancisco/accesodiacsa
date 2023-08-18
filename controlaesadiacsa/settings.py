a5d16337aaae0a23a63c9f2759553dce90e8e77a
gonzalesluisfrancisco <gonzales.luisf@gmail.com>
2023-08-18 11:23:05 -0500

    Update settings.py
    comment STATIC DIR

--- controlaesadiacsa/settings.py
+++ controlaesadiacsa/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_URL = '/static/'
 
 # Default primary key field type
