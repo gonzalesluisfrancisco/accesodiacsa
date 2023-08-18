cea88213a5093672afb419eeae1ca2b2ef660b14
gonzalesluisfrancisco <gonzales.luisf@gmail.com>
2023-08-18 09:51:10 -0500

    Delete startup.sh
    deleting start in folder

--- controlaesadiacsa/startup.sh
+++
@@ -1,5 +0,0 @@
-python manage.py collectstatic
-python manage.py migrate
-gunicorn --workers 2 --threads 4 --timeout 60 --access-logfile \
-    '-' --error-logfile '-' --bind=0.0.0.0:8000 \
-     --chdir=/home/site/wwwroot azureproject.wsgi
