22e7e4d4e8a77cc1fae53bfa0994922433586587
gonzalesluisfrancisco <gonzales.luisf@gmail.com>
2023-08-28 16:01:13 -0500

    Update settings.py
    Storage static sin manifest

--- controlaesadiacsa/settings.py
+++ controlaesadiacsa/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
-DEBUG = True
+DEBUG = False
 
 if 'CODESPACE_NAME' in os.environ:
     CSRF_TRUSTED_ORIGINS = [f'https://{os.getenv("CODESPACE_NAME")}-8000.{os.getenv("GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN")}']
@@ -149,7 +149,7 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     STORAGES = {
     # ...
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
     }
 
