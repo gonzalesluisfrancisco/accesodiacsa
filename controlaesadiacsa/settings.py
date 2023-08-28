166ddee17cbbf071205f0fb6a4c65acb28bba9fb
gonzalesluisfrancisco <gonzales.luisf@gmail.com>
2023-08-28 18:27:55 -0500

    Update settings.py

--- controlaesadiacsa/settings.py
+++ controlaesadiacsa/settings.py
@@ -150,7 +150,7 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     STORAGES = {
     # ...
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
     }
 
