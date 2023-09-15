d0d6d3bf631a814cf78b5b6cb9bbef633efb850e
gonzalesluisfrancisco <gonzales.luisf@gmail.com>
2023-09-15 17:04:19 -0500

    Update settings.py

--- controlaesadiacsa/settings.py
+++ controlaesadiacsa/settings.py
@@ -81,8 +81,7 @@ WSGI_APPLICATION = 'controlaesadiacsa.wsgi.application'
 
 # Configure Postgres database based on connection string of the libpq Keyword/Value form
 # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
-conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+
 
 DATABASES = {
     'default': {
